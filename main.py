c6888a8ad4c49fcffc06f5bf381a922c3f453d25
shimauchi_haruki <tontonkirikiri@gmail.com>
2021-07-18 02:36:01 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#  import_________________________
+import random
+answer = 6
+calulationWay = [+,-,*,/,//]
+randomCalculationWay = random.randint(0,3)
+print(randomCalculationWay)
+result = 
+
+#  def____________________________
+
+def greet():
+    #  greeting & hint of answer, x ** 2 = 6(example)
+    print("Start!!")
+    print("What number is this?")
+    print("Result is", result)
+
+def hint(i):
+    """
+    this method is like hint.
+    first,player enter the number that player think.
+    second,the number change a type to "int".
+    third,calculate
+    finally,display calculation result.
+    if the number is not number,error message display but program do not finish.
+    """
+    localResult = None
+    print(i)
+    tenAnswer = input("Please enter the number that you think. ")
+    #  print(type(tenAnswer))
+    try:
+        intTenAnswer = int(tenAnswer)
+        localResult = intTenAnswer**2 
+        print(localResult)
+    except ValueError:
+       print("that is not number") 
+
+
+def reply():
+    rep = input("What do you reply? ")
+    try:
+        intRep = int(rep)
+        if intRep ** 2 == result:
+            print("correct!!")
+        else:
+            print("incorrect...")
+    except ValueError:
+        print("that is not number")
+
+
+#  main_______________________
+
+
+
+greet()
+
+for elem in [1,2,3]:
+    hint(elem)
+
+reply()
+
+
